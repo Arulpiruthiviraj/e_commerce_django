06f99c9197f10a442919afad844dc3a155a261fe
Arulpiruthiviraj <arul.ceymplon@jeylabs.com.au>
2019-07-17 16:03:50 +0530

    layout added

--- shop/models.py
+++ shop/models.py
@@ -1,9 +1,10 @@
 from django.db import models
+from django.urls import reverse
 
 
 class Category(models.Model):
     name = models.CharField(max_length=150, db_index=True)
-    slug = models.SlugField(max_length=150, unique=True,db_index=True)
+    slug = models.SlugField(max_length=150, unique=True ,db_index=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
@@ -15,8 +16,12 @@ class Category(models.Model):
     def __str__(self):
         return self.name
 
+    def get_absolute_url(self):
+        return reverse('shop:product_list_by_category', args=[self.slug])
+
+
 class Product(models.Model):
-    category = models.ForeignKey('Category', related_name='products', on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, related_name='products', on_delete=models.CASCADE)
     name = models.CharField(max_length=100, db_index=True)
     slug = models.SlugField(max_length=100, db_index=True)
     description = models.TextField(blank=True)
@@ -28,8 +33,11 @@ class Product(models.Model):
     image = models.ImageField(upload_to='products/%Y/%m/%d', blank=True)
 
     class Meta:
-        ordering = ('name',)
+        ordering = ('name', )
         index_together = (('id', 'slug'),)
 
-        def __str__(self):
-            return self.name
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('shop:product_detail', args=[self.id, self.slug])
